1f3228b27c85c19a262f1dd92ad82d4d969857ee
suddicus <suhdenzo04@gmail.com>
2024-02-23 15:14:45 +0100

    Added comments and adjusted code for darts

--- cpp/darts.cpp
+++ cpp/darts.cpp
@@ -1,7 +1,7 @@
 #include "darts.h"
 
 namespace darts {
-    int score(float x, float y){
+    int score(float x, float y){    //By using nested ifs we can quickly traverse down the linear conditions
         if(x*x + y*y <= 100){
             if(x*x + y*y <= 25){
                 if(x*x + y*y <= 1) return 10;
@@ -12,8 +12,3 @@ namespace darts {
         return 0;
     }
 } // namespace darts
-
-int main(){
-    std::cout << darts::score(0.8,-0.8);
-    return 0;
-}
--- cpp/darts.h
+++ cpp/darts.h
@@ -1,7 +1,6 @@
 #if !defined(DARTS_H)
 #define DARTS_H
 
-#include<iostream>
 namespace darts {
     int score(float, float);
 } // namespace darts

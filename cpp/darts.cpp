cd5d862e9fec4223c8821c9cd7b2574c006341e9
suddicus <suhdenzo04@gmail.com>
2024-02-23 15:11:20 +0100

    Completed first iteration of darts problem

--- cpp/darts.cpp
+++ cpp/darts.cpp
@@ -1,5 +1,19 @@
 #include "darts.h"
 
 namespace darts {
-
+    int score(float x, float y){
+        if(x*x + y*y <= 100){
+            if(x*x + y*y <= 25){
+                if(x*x + y*y <= 1) return 10;
+                return 5;
+            }
+            return 1;
+        }
+        return 0;
+    }
 } // namespace darts
+
+int main(){
+    std::cout << darts::score(0.8,-0.8);
+    return 0;
+}
--- cpp/darts.h
+++ cpp/darts.h
@@ -1,8 +1,9 @@
 #if !defined(DARTS_H)
 #define DARTS_H
 
+#include<iostream>
 namespace darts {
-
+    int score(float, float);
 } // namespace darts
 
 #endif //DARTS_H

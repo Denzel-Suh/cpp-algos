051e408e80db983b25b660a0d1e86465560b7ecf
suddicus <suhdenzo04@gmail.com>
2024-02-23 14:10:00 +0100

    Shortened code and added comments

--- cpp/rotational_cipher.cpp
+++ cpp/rotational_cipher.cpp
@@ -5,20 +5,17 @@ namespace rotational_cipher{
         std::string outext {};
         for(char &c: intext){
             if(isalpha(c)){
-                if(isupper(c)){
+                int base = islower(c) ? 'a' : 'A';     // To shorten the solution we use a base and avoid checking for lower and upper as well as use mod so we don't have to compare
+                outext += (c + key - base) % 26 + base;
+                /*if(isupper(c)){                       //Old method redacted to make it shorter
                     (c + key > 90) ? outext += c + key - 26 : outext += c + key;
                 }
                 else if(islower(c)){
                     (c + key > 122) ? outext += c + key - 26 : outext += c + key;
-                }
+                }*/
             }
             else outext += c;
         } 
         return outext;
     }
-}
-
-int main(){
-    std::cout << "WAKAW: " << rotational_cipher::rotate("The quick brown fox jumps over the lazy dog.", 13);
-    return 0;
 }

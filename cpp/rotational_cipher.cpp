80b15d53d1dae16541a2e989d20abd481114188b
suddicus <suhdenzo04@gmail.com>
2024-02-23 13:41:18 +0100

    Implemented preliminary methods

--- cpp/rotational_cipher.cpp
+++ cpp/rotational_cipher.cpp
@@ -2,6 +2,24 @@
 
 namespace rotational_cipher{
     std::string rotate(std::string intext, int key){
-        
+        std::string outext {};
+        int holding{};
+        for(char &c: intext){
+            if(isalpha(c)){
+                if(isupper(c)){
+                    (c + key > 90) ? outext += c + key - 25 : outext += c + key;
+                }
+                else if(islower(c)){
+                    (c + key > 122) ? outext += c + key - 25 : outext += c + key;
+                }
+            }
+            else outext += c;
+        } 
+        return outext;
     }
+}
+
+int main(){
+    std::cout << "WAKAW: " << rotational_cipher::rotate("The quick brown fox jumps over the lazy dog.", 13);
+    return 0;
 }

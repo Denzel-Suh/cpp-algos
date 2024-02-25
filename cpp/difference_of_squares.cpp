bf8e84099acb43c8552b779645dcb38aa927a1d4
suddicus <suhdenzo04@gmail.com>
2024-02-25 21:58:33 +0100

    Re-adjustments made since test cases failed

--- cpp/difference_of_squares.cpp
+++ cpp/difference_of_squares.cpp
@@ -5,13 +5,15 @@ namespace difference_of_squares {
     int sum{};
     int square_of_sum(int n){
         for(i = 1; i <= n; i++){
-            sum += n;
+            sum += i;
+            std::cout << "sum is: " << sum << " and i is: " << i << std::endl;
         }
         return sum * sum;
     }
     int sum_of_squares(int n){
         for(i = 0; i <= n; i++){
-            sum += n * n;
+            sum += i * i;
+            std::cout << "sum is: " << sum << " and i is: " << i << std::endl;
         }
         return sum;
     }
@@ -19,3 +21,8 @@ namespace difference_of_squares {
         return square_of_sum(n) - sum_of_squares(n);
     }
 }  // namespace difference_of_squares
+
+int main(){
+    std::cout << "[Testing 123]" << difference_of_squares::square_of_sum(5) << std::endl;
+    return 0;
+}

428864c79931a5e714f58915ba48f6e2a0bbd646
suddicus <suhdenzo04@gmail.com>
2024-02-27 22:39:42 +0100

    Modified code

--- cpp/trinary.cpp
+++ cpp/trinary.cpp
@@ -2,16 +2,22 @@
 
 namespace trinary {
     int to_decimal(std::string tri){
-        int deci{};
-        int *p = &deci;
+        int *deci;
+        int *p = deci;
+        std::cout << "p is: " << p << " | *p is: " << *p;
         for(char c: tri){
             if((int)c > 2) return 0;
-            p = (int)c;
+            *p = c;
             p++;
         }
+        for(int i = 0; i < tri.length(); i++){
+            std::cout << "Woody : " << deci[i] << std::endl;
+        }
+        return 1;
     }
 }  // namespace trinary
 
 int main(){
+    trinary::to_decimal("123");
     return 0;
 }

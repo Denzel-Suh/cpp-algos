c9af581c5d4cd4a233226dd9e2f35bc7ed90674b
suddicus <suhdenzo04@gmail.com>
2024-02-27 23:16:00 +0100

    Stripped unneccessary code off

--- cpp/trinary.cpp
+++ cpp/trinary.cpp
@@ -2,17 +2,13 @@
 
 namespace trinary {
     int to_decimal(std::string tri){
-        int deci[10], pb{};
-        int *p = deci;
+        int fnum{}, *p = &fnum, count{}, result{};
         for(char c: tri){
-            pb = c - '0';
-            if(pb > 2) std::cout << "Lazlo\n";
-            std::cout << "deci is " << deci[0] << " and &deci is " << &deci << " p is " << p << " and *p is " << *p << std::endl;
-            *p = pb;
-            p++;
+            *p = c - '0';
+            p++, count++;
         }
-        for(int i = 0; i < tri.length(); i++){
-            std::cout << "Woody : " << deci[i] << std::endl;
+        for(int i = count; i >= 0; i--){
+            result = 
         }
         return 1;
     }

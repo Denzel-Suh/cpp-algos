355da3e53f383570671cc6dd5185f269e33afd27
suddicus <suhdenzo04@gmail.com>
2024-02-27 22:27:42 +0100

    Trying pointer approach:

--- cpp/trinary.cpp
+++ cpp/trinary.cpp
@@ -2,10 +2,12 @@
 
 namespace trinary {
     int to_decimal(std::string tri){
-        i
+        int deci{};
+        int *p = &deci;
         for(char c: tri){
             if((int)c > 2) return 0;
-
+            p = (int)c;
+            p++;
         }
     }
 }  // namespace trinary

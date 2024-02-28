ab915a512b5ca2a9910f08b7360040fcbd5a065a
suddicus <suhdenzo04@gmail.com>
2024-02-28 22:27:33 +0100

    Added comments and condition to check for non-trinary numbers

--- cpp/trinary.cpp
+++ cpp/trinary.cpp
@@ -3,11 +3,12 @@
 namespace trinary {
     int to_decimal(std::string tri){
         int fnum[15], *p = fnum, count{}, result{};
-        for(char c: tri){
+        for(char c: tri){               //Use a loop to go through the string
+            if(c > 50) return 0;        //Checks if character is greater than 2, hence not a trinary and returns 0
             *p = c - '0';
             p++, count++;
         }
-        p = fnum;
+        p = fnum;                       //re-initialize the pointer location
         for(int i = 0; i < count; i++){
             result += *(p + i) * pow(3, count - i - 1);
         }

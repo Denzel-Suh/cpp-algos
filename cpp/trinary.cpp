0389910c423a4bd620641474249773c64dc2d666
suddicus <suhdenzo04@gmail.com>
2024-02-28 22:12:15 +0100

    Removed int main from trinary.cpp

--- cpp/trinary.cpp
+++ cpp/trinary.cpp
@@ -2,22 +2,17 @@
 
 namespace trinary {
     int to_decimal(std::string tri){
-        int fnum{}, *p = &fnum, count{}, result{};
+        int fnum[15], *p = fnum, count{}, result{};
         for(char c: tri){
             *p = c - '0';
             std::cout << "p| " << p << ":- - -:bizaz| " << *p << std::endl;
             p++, count++;
         }
-        p = &fnum;
+        p = fnum;
         for(int i = 0; i < count; i++){
-            result += *(p + i) * pow(3, i-1);
-            std::cout << "value of i, p+i, *(p+i), power()|" << i << "| " << p+i << "| " << *(p+i) << "| " << pow(3, i) << std::endl;
+            result += *(p + i) * pow(3, count - i - 1);
+            std::cout << "value of i, p+i, *(p+i), power()|" << i << "| " << p+i << "| " << *(p+i) << "| " << pow(3, count - i -1) << std::endl;
         }
         return result;
     }
 }  // namespace trinary
-
-int main(){
-    std::cout << "102|\n" <<trinary::to_decimal("102");
-    return 0;
-}

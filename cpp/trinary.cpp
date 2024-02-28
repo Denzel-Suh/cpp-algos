8362c5ed906d9a00f549fd3cf6e714090a8e9aa5
suddicus <suhdenzo04@gmail.com>
2024-02-28 22:16:08 +0100

    Cleaned up trinary.cpp

--- cpp/trinary.cpp
+++ cpp/trinary.cpp
@@ -5,13 +5,11 @@ namespace trinary {
         int fnum[15], *p = fnum, count{}, result{};
         for(char c: tri){
             *p = c - '0';
-            std::cout << "p| " << p << ":- - -:bizaz| " << *p << std::endl;
             p++, count++;
         }
         p = fnum;
         for(int i = 0; i < count; i++){
             result += *(p + i) * pow(3, count - i - 1);
-            std::cout << "value of i, p+i, *(p+i), power()|" << i << "| " << p+i << "| " << *(p+i) << "| " << pow(3, count - i -1) << std::endl;
         }
         return result;
     }

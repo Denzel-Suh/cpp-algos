46ee8f0e751d8e81b2c212132a6baa5eed424c7a
suddicus <suhdenzo04@gmail.com>
2024-03-03 22:58:32 +0100

    Added function to make only digits appear

--- cpp/luhn.cpp
+++ cpp/luhn.cpp
@@ -2,6 +2,11 @@
 
 namespace luhn {
     bool valid(std::string card_no){
-
+        std::string encoded{};
+        for(char c: card_no){
+            if(isdigit(c)){
+                encoded += c;
+            }
+        }
     }
 }  // namespace luhn

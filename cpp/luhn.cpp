70a5618d8a580d57245ad507672c8f6cda65fcab
suddicus <suhdenzo04@gmail.com>
2024-03-03 23:03:24 +0100

    Have managed to get string to integer now time to test it

--- cpp/luhn.cpp
+++ cpp/luhn.cpp
@@ -2,11 +2,12 @@
 
 namespace luhn {
     bool valid(std::string card_no){
-        std::string encoded{};
+        std::string clean_no{};
         for(char c: card_no){
             if(isdigit(c)){
-                encoded += c;
+                clean_no += c;
             }
         }
+        int card = atoi(clean_no.to_str());
     }
 }  // namespace luhn

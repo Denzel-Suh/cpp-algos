35894d9cf28faeff2c630beeb4b62c1375902fe6
suddicus <suhdenzo04@gmail.com>
2024-03-06 17:17:17 +0100

    Trying a new but similar approach to solve the problem

--- cpp/luhn.cpp
+++ cpp/luhn.cpp
@@ -1,6 +1,22 @@
 #include "luhn.h"
 
-namespace luhn {
+namespace luhn{
+    bool valid(std::string rawtext){
+        std::string card_no{};
+        for(char c: rawtext)
+            if(isdigit(c)) card_no += c;
+        int p{0}, sum{0};       //Placeholder for mod operation is p and sum holds sum of all digits
+        for(int i = card_no.length(); i >= 0; i--){
+            card_no.length() % 2 == 0 ? p = 0 : p = 1;
+            if(i % 2 == p) (card_no[i] - '0') * 2 > 9 ? card_no[i] = (card_no[i] * 2) - '0' - 9 : card_no[i] = (card_no[i] - '0') * 2;
+            sum += card_no[i] - '0';
+        }
+        if(sum % 10 == 0) return true;
+        return false;
+    }
+}
+
+namespace luhn2 {
     bool valid(std::string card_no){
         int cards[16]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, count{0}, sum{0};
         std::string clean_no{};

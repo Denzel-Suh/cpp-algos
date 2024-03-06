afaec28d185fccb6842a14b6f32f37fd0ebf6b28
suddicus <suhdenzo04@gmail.com>
2024-03-06 19:09:11 +0100

    Fully finished with method

--- cpp/luhn.cpp
+++ cpp/luhn.cpp
@@ -3,19 +3,25 @@
 namespace luhn{
     bool valid(std::string rawtext){
         std::string card_no{};
-        for(char c: rawtext)
+        if(rawtext.length() < 2) return false;
+        for(char c: rawtext){
             if(isdigit(c)) card_no += c;
+            if(!isdigit(c) && c != ' ') return false;
+        }
+        if(card_no.length() < 2) return false;
         int p{0}, sum{0};       //Placeholder for mod operation is p and sum holds sum of all digits
         for(int i = card_no.length() - 1; i >= 0; i--){
             card_no.length() % 2 == 0 ? p = 0 : p = 1;
-            // std::cout << "Card length = " << card_no.length() << "...\tp = " << p << " and card_no[i] is " << card_no[i] << std::endl;
-            if(i % 2 == p) (card_no[i] - '0') * 2 > 9 ? card_no[i] = (card_no[i] * 2) - '0' - 9 : card_no[i] = (card_no[i] - '0') * 2;
+            // std::cout << "p = " << p << " and card_no[i] is " << card_no[i] << std::endl;
+            if(i % 2 == p) (card_no[i] - '0') * 2 > 9 ? card_no[i] = (card_no[i] * 2) - '0' - 9 : card_no[i] = (card_no[i] - '0') * 2 + '0';
+            std::cout << "Card_no[" << i << "] = " << card_no[i] << std::endl; 
             sum += card_no[i] - '0';
         }
-        if(sum % 10 == 0 && sum / 10 > 0) return true;
+        std::cout << "\n\nIntelecto sumo a: " << sum << "\n\n\t";
+        if(sum % 10 == 0) return true;
         return false;
     }
-}
+}                 
 
 namespace luhn2 {
     bool valid(std::string card_no){
@@ -43,6 +49,6 @@ namespace luhn2 {
 }  // namespace luhn
 
 int main(){
-    std::cout << luhn::valid("059") << std::endl;
+    std::cout << luhn::valid("0000 0") << std::endl;
     return 0;
 }

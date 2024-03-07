0cd6bdbe35f9b642abcd079f1b16c90f92d8449e
suddicus <suhdenzo04@gmail.com>
2024-03-07 21:11:33 +0100

    Started building method

--- cpp/hexadecimal.cpp
+++ cpp/hexadecimal.cpp
@@ -2,6 +2,9 @@
 
 namespace hexadecimal {
     int decimal(std::string text){
-        
+        for(char c: text){
+            if(!isdigit(c) && !(c > 96 && c < 103)) return 0;
+        }
+        return 0;
     }
 }  // namespace hexadecimal

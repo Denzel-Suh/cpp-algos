e989a620631b6658d053915fa050d8ea7cf70f57
suddicus <suhdenzo04@gmail.com>
2024-03-08 22:59:43 +0100

    First try at conversion

--- cpp/hexadecimal.cpp
+++ cpp/hexadecimal.cpp
@@ -2,8 +2,12 @@
 
 namespace hexadecimal {
     int decimal(std::string text){
+        int value{0};
+        char p{};
         for(char c: text){
             if(!isdigit(c) && !(c > 96 && c < 103)) return 0;
+            isdigit(c) ? p = '0' : p = 'W';
+            value = value * 16 + c - p;
         }
         return 0;
     }

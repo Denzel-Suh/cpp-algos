0ef7a433270b768a614898c4be5954809084fc53
suddicus <suhdenzo04@gmail.com>
2024-03-09 09:26:06 +0100

    Made some corrections

--- cpp/reverse_string.cpp
+++ cpp/reverse_string.cpp
@@ -5,8 +5,8 @@ namespace reverse_string {
         char temp{};
         for(int i = 0; i < text.length()/2; i++){
             temp = text[i];
-            text[i] = text[text.length() - i];
-            text[text.length() - i] = temp;
+            text[i] = text[text.length() - i - 1];
+            text[text.length() - i - 1] = temp;
         }
         return text;
     }

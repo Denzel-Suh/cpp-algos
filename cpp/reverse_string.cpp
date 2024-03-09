bdf9cb4260d090258da587b87d128134b26b0256
suddicus <suhdenzo04@gmail.com>
2024-03-09 09:22:12 +0100

    First try

--- cpp/reverse_string.cpp
+++ cpp/reverse_string.cpp
@@ -1,5 +1,13 @@
 #include "reverse_string.h"
 
 namespace reverse_string {
-
+    std::string reverse_string(std::string text){
+        char temp{};
+        for(int i = 0; i < text.length()/2; i++){
+            temp = text[i];
+            text[i] = text[text.length() - i];
+            text[text.length() - i] = temp;
+        }
+        return text;
+    }
 }  // namespace reverse_string

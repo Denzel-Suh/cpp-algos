94e54ebc862e772d61cbcf2afd4ff9589016c1b8
sudddicus <suhdenzo04@gmail.com>
2024-03-14 08:20:46 +0100

    first function of making the grade working

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 int main(){
-    vector<int> test = ;
-    for(auto i : )
-    cout << "Results:"
+    vector<double> round{12.5, 15.7, 98.3,9.9,10.9};
+    vector<int> test = round_down_scores(round);
+    for(const auto &i : test) cout << "Results: " << i << endl;;
+    return 0;
 }

82d70389a4122b06cdebeed365ccb8aebc358118
sudddicus <suhdenzo04@gmail.com>
2024-03-17 19:35:54 +0100

    rerunning tests

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     vector<double> round{40.5, 15.7, 98.3,9.9,10.9};
     vector<int> test = round_down_scores(round);
-    // for(const auto &i : test) cout << "Results: " << i << endl;
+    for(const auto &i : test) cout << "Results: " << i << endl;
     // cout << test.size() << "~\nMoe moe kyun~\n\t";
     test = above_threshold(test, 40);
     // cout << test.size() << "~\nMoe moe kyun~\n\t";

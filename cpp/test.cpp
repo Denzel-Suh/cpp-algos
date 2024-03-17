3a3cbc9f156726bf0fc5ccf046d4f5927b11fda0
sudddicus <suhdenzo04@gmail.com>
2024-03-17 19:37:56 +0100

    Finaly moe moe kyun

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -7,9 +7,9 @@ int main(){
     vector<double> round{40.5, 15.7, 98.3,9.9,10.9};
     vector<int> test = round_down_scores(round);
     for(const auto &i : test) cout << "Results: " << i << endl;
-    cout << test.size() << "~\nMoe moe kyun~\n\t";
+    // cout << test.size() << "~\nMoe moe kyun~\n\t";
     test = above_threshold(test, 40);
-    cout << test.size() << "~\nMoe moe kyun~\n\t";
+    // cout << test.size() << "~\nMoe moe kyun~\n\t";
     for(const auto &i : test) cout << "Results: " << i << endl;
     return 0;
 }

67a3a1755bce9f4234be143f6ef6bf4f7e9a72fb
sudddicus <suhdenzo04@gmail.com>
2024-04-19 17:56:53 +0100

    Day of reset. Need to back up

--- cpp/bob.cpp
+++ cpp/bob.cpp
@@ -1,5 +1,14 @@
 #include "bob.h"
 
 namespace bob {
-
+    std::string hey(std::string statement){
+        bool all_upper{}, is_quest{false}, valid_ws{true};
+        int ws_count{}, lettercount
+        if(statement.back() == '?') is_quest = true;
+        for(const auto &c: statement){
+            if(isalph)
+        }
+        std::cout << "1. " << all_upper << " 2. " << is_quest << " 3. " << sizeof(char) << std::endl;
+        return ""; 
+    }
 }  // namespace bob
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include "making_the_grade.cpp"
+#include "bob.cpp"
 
 using namespace std;
 
 int main(){
-    // vector<double> round{40.5, 15.7, 98.3,9.9,10.9};
-    // vector<int> test = round_down_scores(round);
-    array<int, 4> ray = letter_grades(100);
-    for(const auto &i : ray) cout << "Results: " << i << endl;
-    // cout << test.size() << "~\nMoe moe kyun~\n\t";
-    // test = above_threshold(test, 40);
-    // cout << test.size() << "~\nMoe moe kyun~\n\t";
-    // for(const auto &i : test) cout << "Results: " << i << endl;
+    bob::hey("HEY BOY");
     return 0;
 }
